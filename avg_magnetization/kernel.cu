ff503e4ed63372d10090ca6af7b0d45ef1f83809
Pedro Ribeiro <pedro_rib2@hotmail.com>
2016-04-20 17:22:27 +0100

    Programa de cálculos dos campos concluído.

--- avg_magnetization/kernel.cu
+++ avg_magnetization/kernel.cu
@@ -1,23 +1,15 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include <cmath>
-#include <cstring>  
 #include <string>
-#include <sstream>  
 #include <thrust/reduce.h>
-#include <curand.h>
-#include <curand_kernel.h>
 #include <thrust/device_vector.h>
-#include <thrust/sort.h>
 #include <cuda_runtime.h>
 #include <cuda.h>
 #include <math.h>
 #include <time.h>
-#include <float.h>
 #include "cu_veclib.cuh"
 
-#define PI = 3.14159
+#define PI 3.14159
 
 using namespace std;
 
@@ -36,7 +28,7 @@ __global__ void init_pos(vec *d_pil_pos, double x_off, double y_off){
 	int i = blockDim.x*blockIdx.x + threadIdx.x;
 	if (i < 50){
 		d_pil_pos[i].x = d_pil_pos[i].x + x_off;
-		d_pil_pos[i].y = d_pil_pos[i].y + x_off;
+		d_pil_pos[i].y = d_pil_pos[i].y + y_off;
 	}
 }
 
@@ -44,8 +36,8 @@ __global__ void init_dist(vec *d_pil_pos, vec *d_coord, vec *d_dist){
 	int i = blockDim.x*blockIdx.x + threadIdx.x;
 	int j = blockDim.y*blockIdx.y + threadIdx.y;
 	int k;
-	if (i < 128 && j < 128){
-		for (k = 0; k < 50; k++)
+	for (k = 0; k < 50; k++){
+		if (i < 128 && j < 128)
 			d_dist[k + 50 * i + 50 * 128 * j] = d_coord[i + 128 * j] - d_pil_pos[k];
 	}
 }
@@ -53,19 +45,23 @@ __global__ void calc_H(vec *d_dist, vec *d_dip, vec *d_Hi_inc, vec *d_Hi_tot, in
 	int i = blockDim.x*blockIdx.x + threadIdx.x;
 	int j = blockDim.y*blockIdx.y + threadIdx.y;
 	int k;
-	if (i < 128 && j < 128){
-		for (k = 0; k < 50; k++)
+	for (k = 0; k < 50; k++){
+		if (i < 128 && j < 128)
 			d_Hi_inc[k + 50 * i + 50 * 128 * j] = (double)3 * d_dist[k + 50 * i + 50 * 128 * j] * (d_dip[k] * d_dist[k + 50 * i + 50 * 128 * j]) / pow(d_dist[k + 50 * i + 50 * 128 * j].abs(), 5) - d_dip[k] / pow(d_dist[k + 50 * i + 50 * 128 * j].abs(), 3);
 	}
-	if (i < 128 * 128 * 50){
-		d_keys[i] = 1+i/(int)50;
-	}
+}
+
+__global__ void index_keys(int *d_keys){
+	int i = blockDim.x*blockIdx.x + threadIdx.x;
+	if (i < 128 * 128 * 50)
+		d_keys[i] = 1 + i / (int)50;
 }
 
 
 int main(){
 	// PLACA GRÁFICA: NVIDIA GEFORCE 820M - COMPUTE CAPABILITY: 2.1
 
+	clock_t at, bt;
 	int ndip = 50; //# de dipolos
 	int ni = 128; //# de threads (linhas da matriz)
 	int nj = 128*ndip; //# de blocos (linhas da matriz*dipolos do pilar = 6400)
@@ -74,10 +70,12 @@ int main(){
 
 	int threadsPerBlock = ni;
 	int BlocksPerGrid = nj;
+	dim3 tpb(16, 16);
+	dim3 bpg(80, 80);
 	double x_off=0.0005, y_off=0.0005;
 
 	vec *h_dip, *h_pil_pos, *h_Hi_inc, *temp_dip;
-	int *h_dist;
+	vec *h_dist;
 	vec h_Hi_avg(0, 0, 0);
 
 
@@ -86,7 +84,7 @@ int main(){
 	temp_dip = (vec*)malloc(sizeof(vec)*10); //Vector auxiliar
 	h_pil_pos = (vec*)malloc(sizeof(vec)*ndip); //Posição dos dipolos
 	h_Hi_inc = (vec*)malloc(sizeof(vec)*ni*ni); //Valor do campo incidente num elemento de área 
-	h_dist = (int*)malloc(sizeof(int)*ni*ni);
+	h_dist = (vec*)malloc(sizeof(vec)*ni*nj);
 	
 	int *d_keys, *d_rest;
 	vec *d_dist, *d_pil_pos, *d_Hi_inc, *d_dip, *d_coord, *d_Hi_tot;
@@ -95,7 +93,7 @@ int main(){
 	cudaMalloc(&d_Hi_avg, sizeof(vec));
 	cudaMalloc(&d_dist, sizeof(vec)*ni*nj);
 	cudaMalloc(&d_keys, sizeof(int)*ni*nj);
-	cudaMalloc(&d_rest, sizeof(int));
+	cudaMalloc(&d_rest, sizeof(int)*ni*ni);
 	cudaMalloc(&d_pil_pos, sizeof(vec)*ndip);
 	cudaMalloc(&d_Hi_inc, sizeof(vec)*ni*nj);
 	cudaMalloc(&d_Hi_tot, sizeof(vec)*ni*ni);
@@ -108,6 +106,8 @@ int main(){
 	thrust::device_ptr<int> rest_thrust = thrust::device_pointer_cast(d_rest);
 
 	cout << "POWERED BY CUDA" << endl << endl;
+	
+	at = clock();
 
 	//_sleep(2000);
 
@@ -123,8 +123,6 @@ int main(){
 
 		//------- PARSING (Entrada de dados) -------//
 
-		// O delimitador é qq caracter que não seja um número válido (para ints e floating points de qq precisão)
-
 		fileplace_def = "C:\\Users\\Pedro\\Documents\\MATLAB\\dados\\defs_"+to_string(i)+".txt";
 		fileplace_vec = "C:\\Users\\Pedro\\Documents\\MATLAB\\dados\\vecs_"+to_string(i)+".txt";
 
@@ -139,8 +137,7 @@ int main(){
 
 		filein_vec.close();
 
-		for (j = 0; j < ndip; j++){
-			k++;
+		for (j = 0; j < ndip; j++,k++){
 			if (k == 10)
 				k = 0;
 			filein_def >> h_pil_pos[j].x >> h_pil_pos[j].y >> h_pil_pos[j].z;
@@ -156,24 +153,26 @@ int main(){
 
 		//-----------INICIALIZAÇÃO------------------//
 
-		init_map << <BlocksPerGrid, threadsPerBlock >> >(d_coord);
+		init_map << <bpg, tpb >> >(d_coord);
 		init_pos << <BlocksPerGrid, threadsPerBlock >> >(d_pil_pos, x_off, y_off);
-		init_dist << <BlocksPerGrid, threadsPerBlock >> >(d_pil_pos, d_coord, d_dist);
+		init_dist << <bpg, tpb >> >(d_pil_pos, d_coord, d_dist);
 
 		//------ CÁLCULO ------ //
-		calc_H << <BlocksPerGrid, threadsPerBlock >> >(d_dist, d_dip, d_Hi_inc, d_Hi_tot, d_keys);
-		
+		calc_H << <bpg, tpb >> >(d_dist, d_dip, d_Hi_inc, d_Hi_tot, d_keys);
+		index_keys << <BlocksPerGrid, threadsPerBlock >> >(d_keys);
+		cudaDeviceSynchronize();
 		thrust::reduce_by_key(keys_thrust, keys_thrust + ni * nj, Hi_inc_thrust, rest_thrust, Hi_tot_thrust);
 
+		bt = (clock() - at) / CLOCKS_PER_SEC;
 		//------ DATA TRANSFER (D -> H) --------//
 
-		//cudaMemcpy(h_dist, d_keys, sizeof(int)*ni*nj, cudaMemcpyDeviceToHost); //UNCOMMENT TO DIAGNOSE
+		//cudaMemcpy(h_dist, Hi_tot_inc, sizeof(vec)*ni*nj, cudaMemcpyDeviceToHost); //UNCOMMENT TO DIAGNOSE
 		ofstream fileout_points;
 		fileplace_points = "C:\\Users\\Pedro\\Documents\\CUDA_OUT\\points_" + to_string(i) + ".txt";
 		fileout_points.open(fileplace_points);
-		for (i = 0; i < ni * ni; i++){
-			for (j = 0; j < ndip; j++){
-				fileout_points << keys_thrust[j + i * 50] << "\t";
+		for (i = 0; i < ni; i++){
+			for (j = 0; j < ni; j++){
+				fileout_points << Hi_tot_thrust[j + i * ni] << "\t";
 				h_Hi_avg = h_Hi_avg + Hi_tot_thrust[i];
 			}
 			fileout_points << endl;
@@ -201,8 +200,11 @@ int main(){
 	free(h_pil_pos);
 	free(h_Hi_inc);
 	free(temp_dip);
+
+	at = (clock() - at) / CLOCKS_PER_SEC;
 	
-	cout << "PROGRAMA CORRIDO COM SUCESSO!" << endl << "Prima qualquer tecla para sair..." << endl;
+	cout << "PROGRAMA CORRIDO COM SUCESSO EM " << (double)at + (double)bt << " SEGUNDOS!" << endl;
+	cout << "Tempo de processamento de calculo: " << (double)bt << " segundos." << endl << "Tempo de processamento de I/O: " << at << " segundos." << endl << "Prima qualquer tecla para sair..." << endl;
 	cin.get();
 	return 0;
 }
